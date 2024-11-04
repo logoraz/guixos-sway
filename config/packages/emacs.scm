c64fd7aa889bf061d74bddb9f04936fd0401fdae
Erik P Almaraz <erikalmaraz@fastmail.com>
2024-11-03 16:50:54 -0800

    Update Guie raz emacs package definition.

--- config/packages/emacs.scm
+++ config/packages/emacs.scm
@@ -7,10 +7,9 @@
 
 ;;TODO - doesn't work when exported, only with define-public... why?
 
-;;Note: generate new sha256/base32 via
-;; guix hash -x --serializer=nar .
-;; guix hash -x --serializer=git .
-;; Get commit via git log
+;;; Note: generate new sha256/base32 via
+;;; guix hash -x --serializer=nar .
+;;; Get commit via git log
 (define-public raz-emacs
   (package
    (name "raz-emacs")
@@ -18,11 +17,11 @@
    (source (origin
             (method git-fetch)
             (uri (git-reference
-                  (url "https://codeberg.org/loraz/raz-emacs")
-                  (commit "234f2d366476b02a7e27ca0a776db474cda679cb")))
+                  (url "https://codeberg.org/loraz/raz-emacs.git")
+                  (commit "9a1b495891ba6efc5bbefa4539cdd4c8429e25ec")))
             (hash
              (content-hash
-              "1zlwd1ipr1ag16vsws4xnbcqgwaqm55byscj8rb291ypp9yzp2yz"
+              "09iz01c24vk9vk4bkpvh2bm261xj5s1knsnikq6xssbw34svlpd5"
               sha256))))
    (build-system copy-build-system)
    (home-page "https://codeberg.org/loraz/raz-emacs")
