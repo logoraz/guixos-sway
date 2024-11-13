adc25097f9bd5d5b284ff3f605ea185e83102b0b
Erik P Almaraz <erikalmaraz@fastmail.com>
2024-11-12 21:20:04 -0800

    Add obs streaming

---
+++ config/home/services/streaming.scm
@@ -0,0 +1,20 @@
+;;; Borrowed from:
+;;; https://codeberg.org/daviwil/dotfiles/daviwil/home-services/streaming.scm
+
+(define-module (config home services streaming)
+  #:use-module (nongnu packages video)
+  #:use-module (config packages video)
+  #:use-module (gnu home services))
+
+(define (home-streaming-profile-service config)
+  (list obs-with-cef
+        obs-vaapi))
+
+(define-public home-streaming-service-type
+  (service-type (name 'home-streaming)
+                (description "Packages and configuration for streaming with OBS.")
+                (extensions
+                 (list (service-extension
+                        home-profile-service-type
+                        home-streaming-profile-service)))
+                (default-value #f)))
