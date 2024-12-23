b35a0708baec72593736ef9d6fbcc435f75aa49a
Erik Almaraz <erikalmaraz@fastmail.com>
2024-12-23 09:47:20 -0800

    clean-up.

--- config/system/guixos.scm
+++ config/system/guixos.scm
@@ -1,23 +1,23 @@
 (define-module (config system guixos)
-  #:use-module (ice-9 optargs)                   ; ?
-  #:use-module (ice-9 ftw)                       ; ?
+  #:use-module (ice-9 optargs)                   ;-> substitutes->services
+  #:use-module (ice-9 ftw)                       ;-> wip
   #:use-module (gnu)                             ; ?
   #:use-module (gnu packages cups)               ;-> cups-filters
-  #:use-module (gnu packages ssh)                ; openssh
-  #:use-module (gnu packages file-systems)       ; bcacefs-tools
+  #:use-module (gnu packages ssh)                ;-> openssh
+  #:use-module (gnu packages file-systems)       ;-> bcacefs-tools
   #:use-module (gnu packages package-management) ;-> guix-for-channels
   #:use-module (gnu packages linux)              ;-> brightnessctl,lm-sensors
-  #:use-module (gnu packages audio)              ; bluez-alsa
-  #:use-module (gnu packages xorg)               ; egl-wayland
-  #:use-module (gnu packages wm)                 ; swaylock
-  #:use-module (gnu packages wget)               ; wget
-  #:use-module (gnu packages curl)               ; curl
-  #:use-module (gnu packages version-control)    ; git
-  #:use-module (gnu packages compression)        ; zip,unzip
+  #:use-module (gnu packages audio)              ;-> bluez-alsa
+  #:use-module (gnu packages xorg)               ;-> egl-wayland
+  #:use-module (gnu packages wm)                 ;-> swaylock
+  #:use-module (gnu packages wget)               ;-> wget
+  #:use-module (gnu packages curl)               ;-> curl
+  #:use-module (gnu packages version-control)    ;-> git
+  #:use-module (gnu packages compression)        ;-> zip,unzip
   #:use-module (gnu services guix)               ;-> guix-home-service-type
-  #:use-module (gnu services cups)               ; ?
-  #:use-module (gnu services ssh)                ; ?
-  #:use-module (gnu services xorg)               ; ?
+  #:use-module (gnu services cups)               ;-> cups-service-type
+  #:use-module (gnu services ssh)                ;-> openssh-service-type
+  #:use-module (gnu services xorg)               ;-> screen-locker-service-type
   #:use-module (gnu services desktop)            ; ?
   #:use-module (gnu services networking)         ;-> tor-service-type
   #:use-module (gnu system setuid)               ; ?
@@ -25,17 +25,34 @@
   #:use-module (gnu system nss)                  ; ?
   #:use-module (gnu system keyboard)             ; ?
   #:use-module (gnu bootloader)                  ; ?
-  #:use-module (nongnu packages linux)           ; ?
-  #:use-module (nongnu system linux-initrd)      ; ?
+  #:use-module (nongnu packages linux)           ;-> Modified NF Linux kernel
+  #:use-module (nongnu system linux-initrd)      ;-> Microcode for NF  kernel
+  #:use-module (guix gexp)                       ;-> gexp's local-file
   #:use-module (guix transformations)            ;-> options->transformation
   #:use-module (guix ci)                         ; ?
   #:use-module (guix packages)                   ; ?
   #:use-module (guix download)                   ; ?
   #:use-module (config system guixos-channels)
-  #:use-module (config home guixos-home))
+  #:use-module (config home guixos-home)
+
+  ;; wip guixos-sway-core
+  #:export (%guixos-user-name
+            %guixos-keyboard-layout
+            %guixos-bootloader
+            %guixos-swap-devices
+            %guixos-file-systems
+            %guixos-groups
+            %guixos-users
+            %greetd-conf
+            substitutes->services
+            %guixos-base-services
+            %guixos-base-packages
+            %guixos-sway-core))
 
 
-(define %user-name "loraz")
+;;; operating-system parameters
+
+(define %guixos-user-name "loraz")
 
 (define %guixos-keyboard-layout
   (keyboard-layout "us"))
@@ -86,7 +103,12 @@
                                   "video"))) ;; control video devices
          %base-user-accounts))
 
-;; System Services
+;;; System Services
+
+;; Needed for greetd sway configuration...
+(define %greetd-conf (string-append "/home/loraz/.guixos-sway/"
+                                      "files/sway/sway-greetd.conf"))
+
 ;; Use Package substitutes instead of compiling everything & specify channels
 ;; https://guix.gnu.org/manual/en/html_node/Getting-Substitutes-from-Other-Servers.html
 (define* (substitutes->services config #:optional (channels %guixos-channels))
@@ -109,7 +131,7 @@
               "0j66nq1bxvbxf5n8q2py14sjbkn57my0mjwq7k1qm9ddghca7177")))
            %default-authorized-guix-keys))))
 
-(define %guixos-services
+(define %guixos-base-services
   (cons*
    (service screen-locker-service-type
             (screen-locker-configuration
@@ -128,18 +150,14 @@
              (default-paper-size "Letter")
              (extensions (list cups-filters hplip-minimal))))
 
-   ;; TODO: New - need to look into & configure!!
-   (service tor-service-type)
-
    ;; ssh user@host -p 2222
    (service openssh-service-type
             (openssh-configuration
-             (openssh openssh)
+             (openssh openssh-sans-x)
              (port-number 2222)))
 
-   ;; Set up home configuration
-   (service guix-home-service-type
-            `((,%user-name ,%guixos-home)))
+   ;; TODO: New - need to look into & configure!!
+   (service tor-service-type)
 
    (service greetd-service-type
             (greetd-configuration
@@ -154,24 +172,34 @@
                   ;; issues.guix.gnu.org/65769
                   (greetd-wlgreet-sway-session
                    (sway-configuration
-                    (make-file "../../files/sway/sway-greetd.conf")))))
-               (greetd-terminal-configuration (terminal-vt "2"))
-               (greetd-terminal-configuration (terminal-vt "3"))
-               (greetd-terminal-configuration (terminal-vt "4"))
-               (greetd-terminal-configuration (terminal-vt "5"))
-               (greetd-terminal-configuration (terminal-vt "6"))))))
+                    (local-file %greetd-conf
+                                #:recursive? #t)))))
+               (greetd-terminal-configuration
+                (terminal-vt "2"))
+               (greetd-terminal-configuration
+                (terminal-vt "3"))
+               (greetd-terminal-configuration
+                (terminal-vt "4"))
+               (greetd-terminal-configuration
+                (terminal-vt "5"))
+               (greetd-terminal-configuration
+                (terminal-vt "6"))))))
 
-    ;; See: https://guix.gnu.org/manual/en/html_node/Desktop-Services.html
-    (modify-services %desktop-services
-                     ;; remove gdm-service-type
-                     (delete gdm-service-type)
-                     ;; greetd-service-type provides "greetd" PAM service
-                     (delete login-service-type)
-                     ;; and can be used in place of mingetty-service-type
-                     (delete mingetty-service-type)
-                     (guix-service-type
-                      config =>
-                      (substitutes->services config)))))
+   ;; Set up home configuration
+   (service guix-home-service-type
+            `((,%guixos-user-name ,%guixos-home)))
+
+   ;; See: https://guix.gnu.org/manual/en/html_node/Desktop-Services.html
+   (modify-services %desktop-services
+                    ;; remove gdm-service-type
+                    (delete gdm-service-type)
+                    ;; greetd-service-type provides "greetd" PAM service
+                    (delete login-service-type)
+                    ;; and can be used in place of mingetty-service-type
+                    (delete mingetty-service-type)
+                    (guix-service-type
+                     config =>
+                     (substitutes->services config)))))
 
 ;;; Package Transformations & Packages
 ;; ref: https://guix.gnu.org/manual/en/guix.html#Defining-Package-Variants
@@ -185,7 +213,7 @@
          bluez-alsa
          brightnessctl
          lm-sensors
-         openssh
+         openssh-sans-x
          git
          (list git "send-email")
          curl
@@ -194,10 +222,11 @@
          unzip
          %base-packages))
 
+;;; Define GuixOS Sway Alkaline Ice
 
-(define %guixos
+(define %guixos-sway-core
   (operating-system
-   ;; (inherit guixos-base)
+   ;; (inherit system)
    (host-name "locutus")
    (timezone "America/Los_Angeles")
    (locale "en_US.utf8")
@@ -207,10 +236,9 @@
 
    (firmware (list linux-firmware))
 
-   (initrd microcode-initrd)
-
    ;; Fixes Xorg Lag - https://gitlab.com/nonguix/nonguix/-/issues/212
    ;; for Lenovo ThinkPad X1 Carbon 4th Gen (Type 20FB) Laptop.
+   (initrd microcode-initrd)
    (kernel-arguments (cons "i915.enable_psr=0"
                            %default-kernel-arguments))
 
@@ -226,11 +254,10 @@
 
    (packages %guixos-base-packages)
 
-   (services %guixos-services)
+   (services %guixos-base-services)
 
    ;; Allow resolution of '.local' host names with mDNS.
    (name-service-switch %mdns-host-lookup-nss)))
 
-
-;; Instantiate GuixOS Sway: code name GuixOS Phonon
-%guixos
+;;; Instantiate GuixOS
+%guixos-sway-core
